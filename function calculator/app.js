97a21874448ae8e3d6928c3d8cc04f3fb8325354
Maham alvi <148688757+mahamalvi1718@users.noreply.github.com>
2024-03-13 00:15:47 +0500

    Add files via upload

---
+++ function calculator/app.js
@@ -0,0 +1,20 @@
+
+
+function dabana(e){
+    // console.log(e)
+    document.getElementById('inp').value +=e
+}
+
+function eq(){
+    var a = document.getElementById('inp')
+    a.value = eval(a.value)
+}
+
+function clr(){
+    document.getElementById('inp').value=''
+}
+
+function clr1(){
+    var a = document.getElementById('inp')
+    a.value = a.value.slice(0,-1)
+}
